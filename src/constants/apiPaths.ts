067dc337695cf9d229fe72fa87b824cdb2b6351d
Selcuk Can <selcuk_can@epam.com>
2022-07-18 03:27:23 +0200

    removed paragraph

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://0l0azvtroc.execute-api.eu-central-1.amazonaws.com',
+  product: 'https://yobvxhe9ba.execute-api.eu-central-1.amazonaws.com',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
